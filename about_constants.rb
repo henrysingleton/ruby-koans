9e0d48f30a4b13216c492b91b66620dd2e6de22b
Henry Singleton <henry@Henrys-MacBook-Pro.local>
2014-03-02 21:16:29 +1100

    a bunch more progress

--- about_constants.rb
+++ about_constants.rb
@@ -7,16 +7,16 @@ class AboutConstants < Neo::Koan
   C = "nested"
 
   def test_nested_constants_may_also_be_referenced_with_relative_paths
-    assert_equal __, C
+    assert_equal "nested", C
   end
 
   def test_top_level_constants_are_referenced_by_double_colons
-    assert_equal __, ::C
+    assert_equal "top level", ::C #absolute?
   end
 
   def test_nested_constants_are_referenced_by_their_complete_path
-    assert_equal __, AboutConstants::C
-    assert_equal __, ::AboutConstants::C
+    assert_equal "nested", AboutConstants::C #ohh complete path. so above is defaulted to the top level (root, global etc)
+    assert_equal "nested", ::AboutConstants::C
   end
 
   # ------------------------------------------------------------------
@@ -29,25 +29,25 @@ class AboutConstants < Neo::Koan
 
     class NestedAnimal
       def legs_in_nested_animal
-        LEGS
+        LEGS #needs to be declared to actually inherit, which makes sense. 
       end
     end
   end
 
   def test_nested_classes_inherit_constants_from_enclosing_classes
-    assert_equal __, Animal::NestedAnimal.new.legs_in_nested_animal
+    assert_equal 4, Animal::NestedAnimal.new.legs_in_nested_animal
   end
 
   # ------------------------------------------------------------------
 
   class Reptile < Animal
     def legs_in_reptile
-      LEGS
+      LEGS #ALSO needs to be declared to inherit, which i guess makes sense, even thought properties already inherit automatically? 
     end
   end
 
   def test_subclasses_inherit_constants_from_parent_classes
-    assert_equal __, Reptile.new.legs_in_reptile
+    assert_equal 4, Reptile.new.legs_in_reptile
   end
 
   # ------------------------------------------------------------------
@@ -63,8 +63,10 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_both_nested_and_inherited_constants
-    assert_equal __, MyAnimals::Bird.new.legs_in_bird
+    assert_equal 2, MyAnimals::Bird.new.legs_in_bird
   end
+
+#inheritance has more impportance wich is logical. 
 
   # QUESTION: Which has precedence: The constant in the lexical scope,
   # or the constant from the inheritance hierarchy?
@@ -78,10 +80,12 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_explicit_scoping_on_class_definition
-    assert_equal __, MyAnimals::Oyster.new.legs_in_oyster
+    assert_equal 4, MyAnimals::Oyster.new.legs_in_oyster
   end
 
   # QUESTION: Now which has precedence: The constant in the lexical
   # scope, or the constant from the inheritance hierarchy?  Why is it
   # different than the previous answer?
+  
+  # Because while you are creating a new class that is in the MyAnimals scope, it is still being created FROM the parent Aimal object, which was not in that same scope. 
 end
